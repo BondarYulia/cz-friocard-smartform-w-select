c6cac35756e8e104313856675e7b37e87e5491fd
User 002 <user002@MacBook-Air-User-8.local>
2021-04-14 12:37:43 +0300

    w

--- submit.php
+++ submit.php
@@ -1,49 +1,44 @@
 <?php
+     $order = array ();
+     foreach ($_POST as $key => $value) {
+           if ($value != '') {
+                $order[$key] = $value;
+            }
+      }
+      // Define ip
+      if (!empty($_SERVER['HTTP_CF_CONNECTING_IP'])) {
+          $ip =  $_SERVER['HTTP_CF_CONNECTING_IP'];
+      }  elseif (!empty($_SERVER['HTTP_X_REAL_IP'])) {
+         $ip =  $_SERVER['HTTP_X_REAL_IP'];
+      } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+          $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+      } else {
+          $ip =  $_SERVER['REMOTE_ADDR'];
+      }
 
-$ip = $_SERVER['REMOTE_ADDR'];
-$name = $_POST['name'];
-$phone = $_POST['phone'];
-$campaign_id = '978814'; //Код потока
-$is_smart_form = $_POST['is_smart_form'];
-// $sid1 = $_POST['sid1'];
-// $sid2 = $_POST['sid2'];
+      $ips = explode(",", $ip);
+      $order['ip'] = trim($ips[0]);
 
+      $parsed_referer = parse_url($_SERVER['HTTP_REFERER'], PHP_URL_QUERY);
+      parse_str($parsed_referer, $referer_query);
 
-$order = array (
+      $ch = curl_init();
 
-'campaign_id' => $campaign_id,
-'ip' => $ip,
-'name' => $name,
-'phone' => $phone,
-'is_smart_form' => $is_smart_form,
+      curl_setopt($ch, CURLOPT_URL, 'http://11-cz.urotrin.com/submit' );
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1 );
+      curl_setopt($ch, CURLOPT_POST,           1 );
+      curl_setopt($ch, CURLOPT_HEADER,           0 );
+      curl_setopt($ch, CURLOPT_POSTFIELDS,     http_build_query(array_merge($referer_query, $order)) );
+      curl_setopt($ch, CURLOPT_HTTPHEADER,     array('Content-Type: application/x-www-form-urlencoded'));
 
-// 'sid1' => $sid1,
-// 'sid2' => $sid2
+      $result = curl_exec ($ch);
 
-);
+      $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+      if ($httpCode >= 400) {
+          echo "Submit failed!";
+      } else {
+        header('Content-Type: application/json');
+        echo json_encode($result);
+      }
 
-$ch = curl_init();
-
-curl_setopt($ch, CURLOPT_URL, "http://11-cz.urotrin.com/submit" );
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1 );
-curl_setopt($ch, CURLOPT_POST,           1 );
-curl_setopt($ch, CURLOPT_POSTFIELDS,     http_build_query($order) );
-curl_setopt($ch, CURLOPT_HTTPHEADER,     array('Content-Type: application/x-www-form-urlencoded'));
-
-$result=curl_exec ($ch);
-
-if ($result === 0) {
-	echo "Timeout! Everad CPA API didn't respond within default period!";
-} else {
-	$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-	if ($httpCode === 400) {
-		  echo "Order data is invalid! Order is not accepted!";
-	} else if ($httpCode === 401) {
-		  echo "Order is not accepted! No campaign_id.";
-	} else if ($httpCode === 403) {
-		  echo "Order is not accepted! Restricted GEO. Please submit your order from another GEO.";
-	} else if ($httpCode !== 200) {
-		  echo "Order is not accepted! Invalid or incomplete data. Please contact support.";
-	}
-}
 ?>
